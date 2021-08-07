bb635a9cd5dcb645235f3a3ce1556d13e2839103
Aman Tripathi <59706418+tripathiGithub@users.noreply.github.com>
2021-08-08 04:00:52 +0530

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -113,10 +113,10 @@ if btn and text is not '':
 
 with st.sidebar:
     st.header('Identify Toxic Comments')
-    st.write('The threat of abuse and harassment online means that many people stop expressing themselves and give up on seeking different opinions.This Demo app shows how we can restrict users from posting such toxic comments online')
+    st.write('The threat of abuse and harassment online means that many people stop expressing themselves and give up on seeking different opinions. This Demo app shows how we can restrict users from posting such toxic comments online')
     st.write('Types of Toxicity:')
     for i in ['Toxic','Severe_toxic','Obscene','Threat','Insult','Identity_hate']:
-        st.button(f' \t {i}')
+        st.write(f' -{i}')
     
     with st.beta_expander('Dataset Link'):
         st.write('https://www.kaggle.com/c/jigsaw-toxic-comment-classification-challenge/data')          
